260953afeea68605b0e3dc32388957f62eb782ed
solothfen <93383999+solothfen@users.noreply.github.com>
2021-10-31 10:23:27 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Enter your English name:");
+    char name[]={};
+    
+    scanf("%s",name);
+    printf("%s是二次元",name);
+    
+    return 0;
+ }
+
